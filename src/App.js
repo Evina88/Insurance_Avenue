877597c3816083540c9ffd15d542e1221f364866
Evina Mouselimi <evinamous@gmail.com>
2021-03-29 21:01:23 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './insurance-avenue-logo.jpg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary" sticky="top" >
+        <div className="container">
+          <NavbarBrand href="/">
+            <img src={logo} className="logo" alt="logo" />
+          </NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }
